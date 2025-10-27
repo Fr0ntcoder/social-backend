fe0518e737f7fcaa5b53e9f70df0e153ab3a81a7
Fr0ntcoder <creativ4lk@vk.com>
2025-10-27 18:42:20 +0300

    uploads-file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const multer = require('multer')
+const fs = require('fs')
+const defaultDestination = 'uploads'
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+if(!fs.existsSync(defaultDestination)) {
+  fs.mkdirSync(defaultDestination)
+}
 
-module.exports = router;
+const storage = multer.diskStorage({
+  destination: defaultDestination,
+  filename: function(req,file,cb) {
+      cb(null,file.originalname)
+  }
+})
+
+const uploads = multer({storage})
+
+router.get('/register', (req, res) => {
+  res.send('register')
+})
+
+module.exports = router
