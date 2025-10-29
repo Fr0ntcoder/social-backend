3ac248b87a0db34d2c3034b704bb2b36bc24ec25
Fr0ntcoder <creativ4lk@vk.com>
2025-10-29 19:56:46 +0300

    middleware

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+const authToken = (req, res, next) => {
+  const authHeader = req.headers['authorization']
+
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (!token) {
+    return res.status(401).json({ error: 'Вы не авторизованы' })
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Невалидный токен' })
+    }
+
+    req.user = user
+
+    next()
+  })
+}
+
+module.exports = authToken
