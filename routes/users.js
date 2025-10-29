3dbb69042810338beb0574cc935312e7ee9db5a8
Fr0ntcoder <creativ4lk@vk.com>
2025-10-29 21:42:39 +0300

    create controller user

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 const { userController } = require('../controllers')
+const { authToken } = require('../middleware/auth')
 
-router.get('/current', userController.current)
-router.get('/:id', userController.getById)
-router.put('/:id', userController.update)
+router.get('/current', authToken, userController.current)
+router.get('/:id', authToken, userController.getById)
+router.put('/:id', authToken, userController.update)
 
 module.exports = router
